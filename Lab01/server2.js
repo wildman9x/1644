48ad0bc47ddcd7ff67a390164b620dd5f68d9d0c
Anh Trinh <wildmanyt@gmail.com>
2022-11-25 15:58:33 +0700

    update

--- Lab01/server2.js
+++ Lab01/server2.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
+var fs = require("fs");
+var crypto = require("crypto");
+const csv = require("@fast-csv/parse");
+const { parse } = require("path");
 
 var date = new Date();
 var options = {
@@ -14,15 +18,56 @@ app.use(express.urlencoded({ extended: true }));
 
 // post register
 app.post("/register", (req, res) => {
+  var uuid = crypto.randomBytes(16).toString("hex");
   var name = req.body.name;
   var country = req.body.country;
   var gender = req.body.gender;
   var date = new Date().toLocaleDateString("en-US", options);
+  // save data in csv form
+  fs.writeFileSync(
+    "data.csv",
+    `${uuid},${name},${country},${gender}\n`,
+    { flag: "a" },
+    (err) => {
+      if (err) throw err;
+    }
+  );
+
   res.render("confirm", {
     name: name,
     country: country,
     gender: gender,
   });
+});
+
+app.get("/usersList", (req, res) => {
+  var users = [];
+  fs.createReadStream("data.csv")
+    .pipe(
+      csv.parse({
+        delimiter: ",",
+        headers: ["uuid", "name", "country", "gender"],
+      })
+    )
+    .on("error", (error) => console.error(error))
+    .on("data", (row) => {
+      users.push(row);
+    })
+    .on("end", (rowCount) => {
+      // first column is name
+      // second column is country
+      // third column is gender
+      res.render(
+        "usersList",
+        {
+          users: users,
+        },
+        console.log(`Parsed ${rowCount} rows`),
+        console.log(users),
+        // console log only the name of the users
+        console.log(users.map((user) => user[0]))
+      );
+    });
 });
 
 app.get("/register", (req, res) => {
@@ -46,4 +91,3 @@ app.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}`);
 });
 
-console.log("Server running on port 8080");
